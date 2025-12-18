ccbb9bced980cef6f7119d08656dc0b5ba08a44e
MumtazKhan1985 <mumtazkhan01011985@gmail.com>
2025-12-18 15:32:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Resume360 backend is running"
+    return "Resume360 backend running successfully"
 
-# Railway gunicorn use karta hai, isliye ye optional hai
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
